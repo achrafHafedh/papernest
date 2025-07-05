638c4be7dfa7f9ec1b9246c2ed87329c40af0277
achrafHafedh <achraf.hafedh@easiware.fr>
2025-07-05 22:49:28 +0200

    add config playwright for e2e tests

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests/e2e",
+  testMatch: /.*\.spec\.ts(x)?$/,
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: true,
+  use: {
+    baseURL: "http://localhost:3000",
+    browserName: "chromium",
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+  },
+});
