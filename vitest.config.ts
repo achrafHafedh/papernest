03dfe51db2dc2b1c31340ae354cf374ecce93d01
achrafHafedh <achraf.hafedh@easiware.fr>
2025-07-14 02:51:57 +0200

    Adjust Vitest config to ignore files that shouldn't be included in test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,19 @@ export default defineConfig({
     globals: true,
     setupFiles: ["./.storybook/vitest.setup.ts"],
     exclude: ["node_modules", "playwright.config.ts", "e2e/**"],
+    coverage: {
+      exclude: [
+        "**/*.stories.tsx",
+        "**/*.d.ts",
+        "src/i18n/**",
+        "src/app/api",
+        "**/*.config.*",
+        ".next/**",
+        ".storybook/**",
+        "src/middleware.ts",
+        "src/constants.ts",
+        "src/types.ts",
+      ],
+    },
   },
 });
